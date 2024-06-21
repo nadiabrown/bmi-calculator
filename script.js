f15d181d12e3f33d35ad85ff39e7a475c5752b3c
Nadia <nadiavaleah@gmail.com>
2024-06-21 16:22:07 -0500

    add js so that when the user clicks imperial, the form measurements switch to imperial.

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const heightUnitLabel = document.getElementById("height-unit");
+  const weightUnitLabel = document.getElementById("weight-unit");
+  const unitRadios = document.querySelectorAll(
+    'input[name="measurement-system"]'
+  );
+
+  unitRadios.forEach((radio) => {
+    radio.addEventListener("change", function () {
+      if (this.value === "imperial") {
+        heightUnitLabel.textContent = "in";
+        weightUnitLabel.textContent = "lb";
+      } else {
+        heightUnitLabel.textContent = "cm";
+        weightUnitLabel.textContent = "kg";
+      }
+    });
+  });
+});
